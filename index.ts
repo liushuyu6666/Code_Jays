431250cddcc62eed3d919651daa579512dd42229
shuyu liu <liushuyu6666@gmail.com>
2023-06-06 12:32:10 -0400

    add one comment

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const upload = multer({});
 
 app.use(express.json()); // To parse JSON data in the request body
 
-// TODO: mongodbOperation / mysqlOperation should be have a high level description
+// TODO: it is better to inject MongoClient and MysqlClient or use s3Client at s3Repository.
 const userController = new UserController(databaseType);
 const imageController = new ImageController(
     databaseType,
